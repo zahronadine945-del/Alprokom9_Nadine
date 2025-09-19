e682a6f603e3b3bc335ec10e9426984e977583c2
zahronadine945-del <zahronadine945@gmail.com>
2025-09-19 20:18:44 +0700

    Add files via upload

---
+++ tugas alprokom9 nadine.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nadinealprokom9;
+
+/**
+ *
+ * @author Asus
+ */
+public class NadineAlprokom9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nama: Nadine Aulia Zahro");
+        System.out.println("NRP: 5002251041");
+        System.out.println("Asal Kota: Boyolali");
+        System.out.println("Fun Fact: Orangnya introvert parah");
+        System.out.println("Hobi: Mendengarkan musik");
+    
+}
+}
